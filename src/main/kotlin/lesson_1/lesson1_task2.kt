00d369ce6d034cca540ab2193cd2628fbbcdf4ea
VaiS88 <xvaisx@bk.ru>
2023-11-20 15:36:05 +0300

    KS-1-2 Создал переменные и вывел их в консоль.

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,12 @@
+package lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val textThanksForThePurchase: String = "Спасибо за покупку в нашем интернет магазине!"
+    println(numberOfOrders)
+    println(textThanksForThePurchase)
+    var countWorkers: Int = 2000
+    //println(countWorkers)
+    countWorkers = 2000-1
+    println(countWorkers)
+}
